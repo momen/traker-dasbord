1f0d0aa80dbf66b2e7cd01328310bb0b62816810
unknown <mohammed.abdelbaki96@hotmail.com>
2021-08-15 15:20:00 +0200

    Modules sorted by id edited to be descending by default

--- src/components/pages/ProductManagement/Tags/Tags.js
+++ src/components/pages/ProductManagement/Tags/Tags.js
@@ -158,7 +158,7 @@ function Tags() {
   const [selectedItem, setSelectedItem] = useState("");
   const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
   const [itemToDelete, setItemToDelete] = useState("");
-  const [sortModel, setSortModel] = useState([{ field: "id", sort: "asc" }]);
+  const [sortModel, setSortModel] = useState([{ field: "id", sort: "desc" }]);
   const [openMassDeleteDialog, setOpenMassDeleteDialog] = useState(false);
   const [rowsToDelete, setRowsToDelete] = useState([]);
 
